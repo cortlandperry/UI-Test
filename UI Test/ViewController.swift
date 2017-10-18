00ec4c80365568c9d9e59346d5c998ff59f79fc3
Cortland Perry <Cortland@Cortlands-MacBook-Pro.local>
2017-10-18 14:32:46 -0700

    the app has been created

--- UI Test/ViewController.swift
+++ UI Test/ViewController.swift
@@ -20,6 +20,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
+    
+    @IBAction func sendRight(_ sender: UIButton) {
+        textLabel.text = textLabel.text! + textFieldRight.text!;
+        textFieldRight.text = "";
+    }
+    
+    @IBAction func sendLeft(_ sender: UIButton) {
+        textLabel.text = textLabel.text! + textFieldLeft.text!;
+        textFieldLeft.text = "";
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        textLabel.text = "";
+    }
+    
+    @IBAction func clearVowels(_ sender: UIButton) {
+        let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+        textLabel.text = String((textLabel.text)!.characters.filter({!vowels.contains($0)}));
+        
+        
+    }
+    
+    @IBOutlet weak var textFieldRight: UITextField!
+    
+    @IBOutlet weak var textFieldLeft: UITextField!
 }
 
